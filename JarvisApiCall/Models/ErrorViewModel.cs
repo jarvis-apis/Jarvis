2a3e066fd1d427e0a2791b0233cb6ba48a010582
Suraj <suraj@vesuviois.com>
2018-08-03 11:52:49 +0545

    added demo project
    - added c# example for jarvis api call

---
+++ JarvisApiCall/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JarvisApiCall.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ JarvisApiCall/Models/Instruction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JarvisApiCall.Models
+{
+    public class Instruction
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
